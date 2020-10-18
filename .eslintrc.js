11fac3613756417c924d6a2b8fcd5bf5f0c4f14d
Ronny Hsieh <1020ronny@gmail.com>
2020-10-18 17:00:49 +0800

    ts init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,6 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/recommend', '@vue/typescript', '@vue/prettier', '@vue/airbnb'],
   parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    parser: '@typescript-eslint/parser'
   }
-};
+}
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'auto',
+  spaceBeforeFunctionParen: true,
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false
+}
